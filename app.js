0a6ac69d7a72d0c84b346531cfde518db83a9eba
JavelinSx <74298153+JavelinSx@users.noreply.github.com>
2022-10-20 18:24:31 +0300

    complete test postman

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { createUser } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
-
+app.post('/users', createUser);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
---
+++ routes/users.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+const { createUser } = require('../controllers/users');
+
+router.post('/users', createUser);
