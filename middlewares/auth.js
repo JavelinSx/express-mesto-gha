0f743b0b543c61390d04ad8464c5c8fbc12ef214
JavelinSx <zuev499@gmail.com>
2022-10-30 12:25:46 +0300

    check error eslint to auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,6 @@
 const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
+const { SECRET_KEY_DEV } = require('../utils/const');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -9,12 +10,12 @@ module.exports = (req, res, next) => {
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.varify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.varify(token, NODE_ENV === 'production' ? JWT_SECRET : SECRET_KEY_DEV);
   } catch (err) {
     return res
       .status(401)
       .send({ message: 'Необходима авторизация' });
   }
   req.user = payload;
-  next();
+  return next();
 };
