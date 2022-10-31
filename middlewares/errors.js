2c773d910b97de6361e915e9f759138232456001
JavelinSx <zuev499@gmail.com>
2022-10-31 13:12:18 +0300

    fixed eslint error

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,11 +1,9 @@
-const { INTERNAL_SERVER } = require('../utils/errors');
-
 module.exports = (err, req, res, next) => {
-  const { statusCode = INTERNAL_SERVER, message } = err;
+  const { statusCode = 500, message } = err;
   res
     .status(statusCode)
     .send({
-      message: statusCode === INTERNAL_SERVER
+      message: statusCode === 500
         ? `err.name = ${err.name} ; err.message = ${err.message} ; Ошибка по умолчанию.`
         : message,
     });
