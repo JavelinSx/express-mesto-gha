4769a91145c9e94c0632291cacdd16d851d833a4
JavelinSx <zuev499@gmail.com>
2022-10-31 19:40:11 +0300

    check test error 01

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,10 +5,16 @@ const { TOKEN_DEV } = require('../utils/const');
 
 module.exports.auth = (req, res, next) => {
   const { token } = req.cookies;
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    next(new BadAuthError('Необходима Авторизация'));
+  }
+
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : TOKEN_DEV);
+    payload = jwt.verify(token || authorization.replace('Bearer ', ''), NODE_ENV === 'production' ? JWT_SECRET : TOKEN_DEV);
   } catch (err) {
     next(new BadAuthError('Необходима Авторизация'));
   }
